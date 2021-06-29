1343decf0318c92420c70bfb574f0b543f9537e5
Kamil Samigullin <kamil@samigullin.info>
2021-06-29 10:00:54 +0300

    define simple loggers

---
+++ src/Simple/PipeLogger.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OctoLab\Observer\Simple;
+
+use OctoLab\Observer\Logger;
+use OctoLab\Observer\Payload;
+use OctoLab\Observer\Type;
+
+class PipeLogger implements Logger
+{
+    public function log(Type\Severity $severity, string $message, ?Payload\Context $context = null): void
+    {
+    }
+}
